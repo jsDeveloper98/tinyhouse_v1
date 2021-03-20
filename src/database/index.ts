62502d9f58a5c013f6e8479030d2f3b182fb24c1
jsDeveloper98 <boyakhchyan1010@gmail.com>
2021-03-21 00:24:09 +0400

    db types

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,5 @@
 import { MongoClient } from "mongodb";
-import { Database } from "../lib/types";
+import { Database, User, Listing, Booking } from "../lib/types";
 
 const { DB_USER, DB_USER_PASSWORD, DB_CLUSTER } = process.env;
 const URL = `mongodb+srv://${DB_USER}:${DB_USER_PASSWORD}@${DB_CLUSTER}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -13,6 +13,8 @@ export const connectDatabase = async (): Promise<Database> => {
   const db = client.db("main");
 
   return {
-    listings: db.collection("test_listings"),
+    bookings: db.collection<Booking>("bookings"),
+    listings: db.collection<Listing>("listings"),
+    users: db.collection<User>("users"),
   };
 };
